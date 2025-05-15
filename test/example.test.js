ea1386649c8c395e3236ad5ee6e6bbf029788847
ruizalec <ingalex140797@gmail.com>
2025-05-15 15:35:25 -0700

    prueba 2 de ejecucion con mocha

---
+++ test/example.test.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+describe('Mi prueba de navegador', () => {
+    it('Debería abrir una página web', async () => {
+        // Aquí puedes agregar tu código de prueba
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto('htpp://example.com');
+        await browser.close();
+    })
+    it('Debería abrir una página web x2', async () => {
+        // Aquí puedes agregar tu código de prueba
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto('htpp://google.com');
+        await browser.close();
+    })
+    it('Debería abrir una página web x3', async () => {
+        // Aquí puedes agregar tu código de prueba
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto('htpp://yahoo.com');
+        await browser.close();
+    })
+})
+
