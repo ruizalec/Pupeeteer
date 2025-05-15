7b14316dd1c21eb7a3926b39b2416f00f235c425
ruizalec <ingalex140797@gmail.com>
2025-05-15 14:54:58 -0700

    prueba con mocha

---
+++ tests/example.test.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+describe('Mi prueba de navegador', () => {
+    it('Debería abrir una página web', async () => {
+        // Aquí puedes agregar tu código de prueba
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto('htpp://example.com');
+        await browser.close();
+    })
+})
+
