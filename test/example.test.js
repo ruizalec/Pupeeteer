6f9d1c8564d88c890d4c3e54b5454c8aeb3ab6ad
ruizalec <ingalex140797@gmail.com>
2025-05-26 15:02:03 -0700

    prueba con pagina nueva

--- test/example.test.js
+++ test/example.test.js
@@ -1,3 +1,4 @@
+const { timeout } = require('puppeteer');
 const puppeteer = require('puppeteer');
 
 describe('Mi prueba de navegador', () => {
@@ -5,14 +6,17 @@ describe('Mi prueba de navegador', () => {
         // Aquí puedes agregar tu código de prueba
         const browser = await puppeteer.launch({ headless: false })
         const page = await browser.newPage()
-        await page.goto('http://automationpractice.com/index.php')
+        await page.goto('http://example.com/index.php')
         await browser.close()
     })
     it('Debería abrir una página web x2', async () => {
         // Aquí puedes agregar tu código de prueba
         const browser = await puppeteer.launch({ headless: false })
         const page = await browser.newPage()
-        await page.goto('htpp://google.com')
+        await page.goto('https://sdmsuplementos.com/collections/advance-nutrition/products/hydrotein-5-lbs')
+        await page.waitForSelector('h1')
+        timeout = 10000
+        const title = await page.title()
         await browser.close()
     })
     it('Debería abrir una página web x3', async () => {
